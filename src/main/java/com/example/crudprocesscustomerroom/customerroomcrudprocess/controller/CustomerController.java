6a0327dbecf066946a6e42437de81619d2acdf4e
tolgay.balci@hotmail.com <tolgay.balci@hotmailcom>
2017-12-31 11:58:41 +0300

    improves customer controller

--- src/main/java/com/example/crudprocesscustomerroom/customerroomcrudprocess/controller/CustomerController.java
+++ src/main/java/com/example/crudprocesscustomerroom/customerroomcrudprocess/controller/CustomerController.java
@@ -3,11 +3,16 @@ package com.example.crudprocesscustomerroom.customerroomcrudprocess.controller;
 import com.example.crudprocesscustomerroom.customerroomcrudprocess.domain.Customer;
 import com.example.crudprocesscustomerroom.customerroomcrudprocess.repository.CustomerRepository;
 import com.example.crudprocesscustomerroom.customerroomcrudprocess.repository.RoomRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@Slf4j
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
@@ -19,39 +24,57 @@ public class CustomerController {
     private RoomRepository roomRepository;
 
     @GetMapping("")
-    public String getCustomerList(Model model){
+    public String getCustomerList(Model model) {
         model.addAttribute("customers", customerRepository.findAll());
         return "customers/customerList";
     }
 
     @RequestMapping("/new")
-    public String getNewCustomer(Model model){
-        model.addAttribute("customer",new Customer());
-        model.addAttribute("rooms",roomRepository.findAll());
+    public String getNewCustomer(Model model) {
+        model.addAttribute("customer", new Customer());
+        model.addAttribute("rooms", roomRepository.findAll());
         return "customers/newCustomer";
     }
 
     @PostMapping("/new")
-    public String postNewCustomer(@ModelAttribute Customer customer){
-        customerRepository.save(customer);
+    public String postNewCustomer(@ModelAttribute @Valid Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.warn("Customer with {} id is not found", customer.getId());
+            return "customers/newCustomer";
+        } else {
+            customerRepository.save(customer);
+        }
         return "redirect:/customers";
     }
 
     @GetMapping("/{id}")
-    public String getShowCustomer(@PathVariable Long id, Model model){
-        model.addAttribute("customer", customerRepository.findById(id).get());
+    public String getShowCustomer(@PathVariable Long id, Model model) {
+        if (!customerRepository.findById(id).isPresent()) {
+            log.warn("Customer with {} id is not found.", id);
+        } else {
+            model.addAttribute("customer", customerRepository.findById(id).get());
+        }
         return "customers/showCustomer";
     }
 
     @GetMapping("/{id}/update")
-    public String getUpdateCustomer(@PathVariable Long id, Model model){
-        model.addAttribute("customer",customerRepository.findById(id).get());
+    public String getUpdateCustomer(@PathVariable Long id, Model model) {
+        if (!customerRepository.findById(id).isPresent()) {
+            log.warn("Customer with {} id is not found", id);
+        } else {
+            model.addAttribute("customer", customerRepository.findById(id).get());
+        }
         return "customers/updateCustomer";
     }
 
     @PostMapping("/{id}/update")
-    public String postUpdateCustomer(@ModelAttribute Customer customer){
-        customerRepository.save(customer);
+    public String postUpdateCustomer(@ModelAttribute @Valid Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.warn("Customer with {} id is not found", customer.getId());
+            return "customers/updateCustomer";
+        } else {
+            customerRepository.save(customer);
+        }
         return "redirect:/customers";
     }
 }
